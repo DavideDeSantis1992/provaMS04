0117f1b7c6db5801185e0a7ff82f9aed959c24e4
Davide <davidedesantis1992@gmail.com>
2024-06-20 16:42:13 +0200

    passaggio informazioni

--- src/main/java/com/example/geodNotifApp/serviceImpl/GeodNotifAppServiceImpl.java
+++ src/main/java/com/example/geodNotifApp/serviceImpl/GeodNotifAppServiceImpl.java
@@ -1,114 +1,106 @@
 package com.example.geodNotifApp.serviceImpl;
+
 import com.example.geodNotifApp.dto.DestinatariDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.example.geodNotifApp.dto.NotificareDTO;
 import com.example.geodNotifApp.service.GeodNotifAppService;
 import com.example.geodNotifApp.webClient.GeodDatiAppClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class GeodNotifAppServiceImpl implements GeodNotifAppService {
+
     @Value("${bodyEmailSuccesso}")
     private String bodyEmailSuccesso;
+
     @Value("${oggettoEmailSuccesso}")
     private String oggettoEmailSuccesso;
+
     @Value("${bodyEmailErrore}")
     private String bodyEmailErrore;
+
     @Value("${oggettoEmailErrore}")
     private String oggettoEmailErrore;
 
     @Value("${gruppo1}")
     private List<String> gruppo1;
+
     @Value("${gruppo2}")
     private List<String> gruppo2;
+
     @Value("${gruppo3}")
     private List<String> gruppo3;
+
     @Value("${gruppo4}")
     private List<String> gruppo4;
 
     private Logger logger = LoggerFactory.getLogger(GeodNotifAppServiceImpl.class);
+
     @Autowired
     private GeodDatiAppClient geodDatiAppClient;
-/*
-    private ResponseEntity<List<NotificareDTO>> listaProcessiDaElaborare() {
-        try{
-            logger.info("chiamo il client");
-            ResponseEntity<List<NotificareDTO>> notificareDTO = geodDatiAppClient.listProcessi();
-            return notificareDTO;
-        }catch(Exception err){
-            // TODO inserire i log
-            logger.error("chiamata client fallita");
-            return ResponseEntity.badRequest().build();
+
+    private void listaProcessiDaElaborare() {
+        try {
+            logger.info("INIZIO PROCESSO----");
+            List<NotificareDTO> lista = geodDatiAppClient.listaProcessi();
+            logger.info("ho preso la lista dei processi per elaborarla: " + lista);
+
+            for (NotificareDTO notificareDTO : lista) {
+                String artifact = notificareDTO.getIdIstanzaProcesso();
+                String frequenza = notificareDTO.getIdFrequenzaCd().getDescrizione().toLowerCase();
+                String tipo = notificareDTO.getIdTipoCd().getDescrizione().toLowerCase();
+                logger.info("artifact: " + artifact);
+                logger.info("frequenza: " + frequenza);
+                logger.info("tipo: " + tipo);
+
+                List<String> destinatari = new ArrayList<>();
+                switch (frequenza) {
+                    case "mensile":
+                        switch (tipo) {
+                            case "infortunio":
+                                destinatari = gruppo1;
+                                break;
+                            case "malattia":
+                                destinatari = gruppo3;
+                                break;
+                        }
+                        break;
+                    case "semestrale":
+                        switch (tipo) {
+                            case "infortunio":
+                                destinatari = gruppo2;
+                                break;
+                            case "malattia":
+                                destinatari = gruppo4;
+                                break;
+                        }
+                        break;
+                }
+
+                String oggettoEmail = oggettoEmailSuccesso;
+                String bodyEmail = bodyEmailSuccesso + ", abbiamo notificato il completamento del processo " + artifact +
+                        " con frequenza " + frequenza + " e tipo " + tipo;
+
+                logger.info("BodyEmail: " + bodyEmail);
+                logger.info("oggettoEmail: " + oggettoEmail);
+                logger.info("destinatari: " + destinatari);
+                logger.info("PROCESSO TERMINATO----");
+            }
+        } catch (Exception err) {
+            logger.error("chiamata client fallita", err);
         }
     }
-    */
-    private String listaProcessiDaElaborare(){
-        try{
-            logger.info("chiamo il client per prendere i processi da elaborare");
-            String url = geodDatiAppClient.listProcessi();
-            return url;
-        }catch(Exception err){
-            // TODO inserire i log
-            logger.error("chiamata client fallita");
-            return null;
-        }
-    }
-    /*
-    private ResponseEntity<List<DestinatariDTO>> listaDestinatariDaElaborare() {
-        try{
-            logger.info("chiamo il client");
-            ResponseEntity<List<DestinatariDTO>> destinatariDTO = geodDatiAppClient.listDestinatari();
-            return destinatariDTO;
-        }catch(Exception err){
-            // TODO inserire i log
-            logger.error("chiamata client fallita");
-            return ResponseEntity.badRequest().build();
-        }
-    }
-     */
-//    private String listaDestinatariDaElaborare(){
-//        try{
-//            logger.info("chiamo il client per prendere i destinatari da elaborare");
-//            String url = geodDatiAppClient.listDestinatari();
-//            return url;
-//        }catch(Exception err){
-//            // TODO inserire i log
-//            logger.error("chiamata client fallita");
-//            return null;
-//        }
-//    }
 
     @Override
     public ResponseEntity<Void> elaborazioneGenerale() {
-//        // ho la lista dei processi con i dati dettagliati
-       listaProcessiDaElaborare();
-//        // ho la lista delle email dei destinatari
-//        listaDestinatariDaElaborare();
-        generaEmail();
-        generaGruppo();
+        listaProcessiDaElaborare();
         return null;
-    }
-
-    public void generaEmail(){
-        String bs = bodyEmailSuccesso;
-        String os = oggettoEmailSuccesso;
-        String be = bodyEmailErrore;
-        String oe = oggettoEmailErrore;
-        logger.info("1)bodyEmailSuccesso: "+bs);
-        logger.info("2)oggettoEmailSuccesso: "+os);
-        logger.info("3)bodyEmailErrore: "+be);
-        logger.info("4)oggettoEmailErrore: "+oe);
-    }
-    public void generaGruppo(){
-        logger.info("gruppo1: "+gruppo1);
-        logger.info("gruppo2: "+gruppo2);
-        logger.info("gruppo3: "+gruppo3);
-        logger.info("gruppo4: "+gruppo4);
     }
 }
--- src/main/java/com/example/geodNotifApp/webClient/GeodDatiAppClient.java
+++ src/main/java/com/example/geodNotifApp/webClient/GeodDatiAppClient.java
@@ -1,5 +1,7 @@
 package com.example.geodNotifApp.webClient;
 import com.example.geodNotifApp.dto.DestinatariDTO;
+import com.example.geodNotifApp.entita.Frequenza;
+import com.example.geodNotifApp.entita.Tipo;
 import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,11 +51,50 @@ public class GeodDatiAppClient {
         }
     }
      */
-    public String listProcessi(){
+    public List<NotificareDTO> listaProcessi(){
         String url = "http://localhost:8085/processo";
         logger.info("ho preso l'url:"+url);
-        return url + " ho preso questo url dal microservizio client MS5 per prendere il processo";
+        // TODO creo i vari TIPI
+        Tipo infortunio = new Tipo();
+        infortunio.setId("1");
+        infortunio.setDescrizione("infortunio");
+
+        Tipo malattia = new Tipo();
+        malattia.setId("2");
+        malattia.setDescrizione("malattia");
+        // TODO creo le varie FREQUENZE
+
+        Frequenza mensile = new Frequenza();
+        mensile.setId("1");
+        mensile.setDescrizione("mensile");
+
+        Frequenza semestrale = new Frequenza();
+        semestrale.setId("2");
+        semestrale.setDescrizione("semestrale");
+
+
+        // TODO creo nuovi processi di NotificareDTO
+        NotificareDTO processo1 = new NotificareDTO();
+        processo1.setIdIstanzaProcesso("1");
+        processo1.setIdTipoCd(infortunio);
+        processo1.setIdFrequenzaCd(mensile);
+        processo1.setIdStatoArtifact(null);
+        processo1.setIdStatoNotifica(null);
+
+        NotificareDTO processo2 = new NotificareDTO();
+        processo2.setIdIstanzaProcesso("2");
+        processo2.setIdTipoCd(malattia);
+        processo2.setIdFrequenzaCd(semestrale);
+        processo2.setIdStatoArtifact(null);
+        processo2.setIdStatoNotifica(null);
+
+        List<NotificareDTO> listaProcessiDTO = List.of(processo1, processo2);
+        logger.info("ho preso la lista dei processi dall'endpoint: "+listaProcessiDTO);
+
+        return listaProcessiDTO;
     }
+
+
 //    public String listDestinatari(){
 //        String url = "http://localhost:8085/destinatari";
 //        logger.info("ho preso l'url:"+url);
